d77b408a6ce44613cb80c0ef0275e6e4d74d2e35
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2024-08-19 02:47:50 -0300

    Criando NAVBAR

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+
 
 }
