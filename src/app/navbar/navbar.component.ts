f9dd07ea6aab67018878933af8719411d84bc669
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2024-08-21 00:18:01 -0300

    Atualização navbar, criação da ficha de anamnese.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  public isSidebar: boolean = false;
 
+  onSave() {
+    this.isSidebar = !this.isSidebar;
+  }
 
 }
